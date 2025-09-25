81932ed6a86693f88d308270ca8a4d52ba4631cf
MOUNEASH-14 <mouneash14@gmail.com>
2025-09-25 14:46:08 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char ch1,ch2;
+    printf("enter the char:");
+    scanf("%c",&ch1);
+    printf("enter the char:");
+    scanf("%c",&ch2);
+    if (ch1==ch2)
+        printf("both chr equal");
+    else
+        printf("both chr not equal");
+    return 0;   
+}
+ 
+	
+ 
+ 
+ 
